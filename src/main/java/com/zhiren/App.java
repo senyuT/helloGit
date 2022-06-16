69a1cd58cb2269b68fde2f209239d541d886a897
zhiRen <1455210915@qq.com>
2022-06-16 13:29:08 +0800

    hot_fix v1

--- src/main/java/com/zhiren/App.java
+++ src/main/java/com/zhiren/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello Git" );
         System.out.println( "Hello Git2" );
         System.out.println( "Hello Git3" );
+        System.out.println( "Hello hot_fix" );
     }
 }
